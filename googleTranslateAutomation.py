eb2ca7d5bc8694e43c2197120d4c0fbb369369f8
fabricius1 <fabriciusbr@gmail.com>
2021-03-10 12:29:52 -0300

    Upload from .txt file added

--- googleTranslateAutomation.py
+++ googleTranslateAutomation.py
@@ -1,7 +1,10 @@
 import webbrowser
+import requests
+import time
 
 
 def parse_string(text):
+    """Replace the following characteres in the text"""
     special_characters = (
             ("%", "%25"),
             (" ", "%20"),
@@ -21,7 +24,7 @@ def parse_string(text):
     return text
 
 
-def open_google_trans(source_language="en", target_language="pt", text=None):
+def open_google_trans(source_language="en", target_language="pt", text_to_translate=None):
     """
         Translate the text from the source_language to the target_language, by opening the
     Google Translate site with this info.
@@ -30,9 +33,16 @@ def open_google_trans(source_language="en", target_language="pt", text=None):
     """
 
     # exit the function if no text is submitted
-    if not text:
+    if not text_to_translate:
         print("No text submitted to translation.\nPlease insert a text.\n")
         return None
+
+    if text_to_translate.startswith("http"):
+        text_to_translate = requests.get(text_to_translate).text
+
+    elif text_to_translate.endswith(".txt"):
+        with open(text_to_translate) as file:
+            text_to_translate = file.read()
 
     # variables to be used in the url:
     # source language
@@ -42,31 +52,20 @@ def open_google_trans(source_language="en", target_language="pt", text=None):
     # operation
     operation = "translate"
     
-    text = parse_string(text)
+    text_to_translate = parse_string(text_to_translate)
 
     # f-string with variables:
-    link = f"https://translate.google.com/?sl={sl}&tl={tl}&text={text}&op={operation}"
+    link = f"https://translate.google.com/?sl={sl}&tl={tl}&text={text_to_translate}&op={operation}"
 
     # This function, from the webbrowser module, opens a link in the default browser
     webbrowser.open(link)
 
 
 if __name__ == "__main__":
-    # Romeo and Juliet's Prologue
-    # (extracted from http://shakespeare.mit.edu/romeo_juliet/full.html) 
-    text = """Two households, both alike in dignity,
-    In fair Verona, where we lay our scene,
-    From ancient grudge break to new mutiny,
-    Where civil blood makes civil hands unclean.
-    From forth the fatal loins of these two foes
-    A pair of star-cross'd lovers take their life;
-    Whose misadventured piteous overthrows
-    Do with their death bury their parents' strife.
-    The fearful passage of their death-mark'd love,
-    And the continuance of their parents' rage,
-    Which, but their children's end, nought could remove,
-    Is now the two hours' traffic of our stage;
-    The which if you with patient ears attend,
-    What here shall miss, our toil shall strive to mend."""
-
-    open_google_trans("en", "pt", text)
+    languages = ["pt", "es", "eo", "la", "tr", "ko", "ja"]
+    url = "https://raw.githubusercontent.com/fabricius1/Google-Translate-Automation/master/textToTranslate.txt"
+    text_to_translate = url
+    
+    for language in languages:
+        open_google_trans("en", language, text_to_translate)
+        time.sleep(5)
