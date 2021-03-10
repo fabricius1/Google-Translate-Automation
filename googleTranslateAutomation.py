4ac701a0433b13d85764ff6c83c880a9dc75d283
fabricius1 <fabriciusbr@gmail.com>
2021-03-10 10:18:00 -0300

    file textToTranslate.txt added

--- googleTranslateAutomation.py
+++ googleTranslateAutomation.py
@@ -21,36 +21,52 @@ def parse_string(text):
     return text
 
 
-# variables to be used in the url:
-# source language
-sl = "en"
-# target language
-tl = "pt"
-# operation
-operation = "translate"
+def open_google_trans(source_language="en", target_language="pt", text=None):
+    """
+        Translate the text from the source_language to the target_language, by opening the
+    Google Translate site with this info.
+        Parameters are all strings.
+        Return is None
+    """
 
-# Romeo and Juliet's Prologue
-# (extracted from http://shakespeare.mit.edu/romeo_juliet/full.html) 
-text = """
-Two households, both alike in dignity,
-In fair Verona, where we lay our scene,
-From ancient grudge break to new mutiny,
-Where civil blood makes civil hands unclean.
-From forth the fatal loins of these two foes
-A pair of star-cross'd lovers take their life;
-Whose misadventured piteous overthrows
-Do with their death bury their parents' strife.
-The fearful passage of their death-mark'd love,
-And the continuance of their parents' rage,
-Which, but their children's end, nought could remove,
-Is now the two hours' traffic of our stage;
-The which if you with patient ears attend,
-What here shall miss, our toil shall strive to mend."""
+    # exit the function if no text is submitted
+    if not text:
+        print("No text submitted to translation.\nPlease insert a text.\n")
+        return None
 
-text = parse_string(text)
+    # variables to be used in the url:
+    # source language
+    sl = source_language
+    # target language
+    tl = target_language
+    # operation
+    operation = "translate"
+    
+    text = parse_string(text)
 
-# f-string with variables:
-link = f"https://translate.google.com/?sl={sl}&tl={tl}&text={text}&op={operation}"
+    # f-string with variables:
+    link = f"https://translate.google.com/?sl={sl}&tl={tl}&text={text}&op={operation}"
 
-# This function, from the webbrowser module, opens a link in the default browser
-webbrowser.open(link)
+    # This function, from the webbrowser module, opens a link in the default browser
+    webbrowser.open(link)
+
+
+if __name__ == "__main__":
+    # Romeo and Juliet's Prologue
+    # (extracted from http://shakespeare.mit.edu/romeo_juliet/full.html) 
+    text = """Two households, both alike in dignity,
+    In fair Verona, where we lay our scene,
+    From ancient grudge break to new mutiny,
+    Where civil blood makes civil hands unclean.
+    From forth the fatal loins of these two foes
+    A pair of star-cross'd lovers take their life;
+    Whose misadventured piteous overthrows
+    Do with their death bury their parents' strife.
+    The fearful passage of their death-mark'd love,
+    And the continuance of their parents' rage,
+    Which, but their children's end, nought could remove,
+    Is now the two hours' traffic of our stage;
+    The which if you with patient ears attend,
+    What here shall miss, our toil shall strive to mend."""
+
+    open_google_trans("en", "pt", text)
